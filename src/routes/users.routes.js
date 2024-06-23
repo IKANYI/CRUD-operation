eb840dee90775b8b31bdb4c7476d372ecd50cb40
IKANYI <paulkaranja214@gmail.com>
2024-06-23 23:22:03 +0300

    setup commad to get all and single user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,12 +3,27 @@ import pool from '../db.config.js'
  
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send("getting all users")
+router.get('/', async(req, res) => {
+  try {
+  const result = await pool.query("SELECT * FROM products")
+  res.status(200).json({ success: true, data: result.rows})
+  } catch (err) {
+    res.status(500).json({success: false, message: err.message})
+  } 
 })
 
-router.get(':id', (req, res) => {
-  res.send("getting a single user")
+router.get('/:id', async(req, res) => {
+  const id = req.params.id
+  try{
+     const result = await pool.query("SELECT * FROM products WHERE id = $1", [id])
+    if (result.rowCount === 0){
+        res.status(404).json({success: false, message: 'Product not found' })
+    }else{
+     res.status(200).json({success: true, data: result.rows[0]})
+    }
+  }catch (err) {
+    res.status(500).json({success: false, message: err.message})
+  }
 })
 
 router.post(':id', (req, res) => {
